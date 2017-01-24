0e2d483228317420a0993ee2bb6526c4d35323b5
Laptop <polpol@walla.co.il>
2017-01-24 22:03:45 +0200

    added power charge/disconnect toast, keyboard at start disappears

---
+++ app/src/main/java/com/example/laptop/finalproject/MyPowerConnectionReceiver.java
@@ -0,0 +1,22 @@
+package com.example.laptop.finalproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+//this receiver class is to give indication if the device was plugged in or disconnected from charging
+public class MyPowerConnectionReceiver extends BroadcastReceiver {
+    public MyPowerConnectionReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+            Toast.makeText(context, "The device is charging", Toast.LENGTH_SHORT).show();
+        } else {
+            intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED);
+            Toast.makeText(context, "The device is not charging/unplugged", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
